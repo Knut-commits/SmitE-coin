5f7b535c4ac7dc79b3da3fc4a27125bd68ed3b54
Knut-commits <26knuto@stu.tes.tp.edu.tw>
2025-03-07 10:49:18 +0800

    fix

--- Blockchain/block.py
+++ Blockchain/block.py
@@ -1,1 +1,14 @@
-print("hello")
+import hashlib
+import time
+import json
+#nonce means number only used once and is a cyrpto currency term.
+class block:
+    def_init_(self,index,previoushash,transactions,timestamp = none,nonce=0):
+        # Block properties
+        self.index = index  # Position of the block in the chain
+        self.previoushash = previoushash  # Hash of the previous block
+        self.transactions = transactions  # List of transactions in the block
+        self.timestamp = timestamp or time.time()  # Timestamp of block creation
+        self.nonce = nonce  # Nonce for Proof of Work
+        self.hash = self.calculate_hash()  # Hash of the block
+    def calculate_hash():

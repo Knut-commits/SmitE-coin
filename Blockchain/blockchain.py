cdc00753c233fecd69899554c499319920f55ebe
Knut-commits <26knuto@stu.tes.tp.edu.tw>
2025-03-11 10:29:48 +0800

    quick restart

--- Blockchain/blockchain.py
+++ Blockchain/blockchain.py
@@ -4,6 +4,8 @@ class Blockchain:
 
 def _init_ (self):
     #we are intiizing the blockchain iwth teh genesis Block(first block)
-    self.chain = [self.create_genesis_block()]
+    self.chain = [self.CreateGenesisBlock()]
     self.pending_transactions= [] #this si teh tranactions that need to be minded, starts empty
-    slef.difficulty= 4 # this si minning diffculty and this menas 4 zeros is reuired at teh end of hash
+    slef.difficulty= 4 # this is minning diffculty and this menas 4 zeros is reuired at teh end of hash
+
+def CreateGenesisBlock

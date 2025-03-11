8f4acb45b502fab14259885932ad757cdb33dcb3
KuzBahadir <kuzbahadir@gmail.com>
2025-03-11 10:37:03 +0800

    fix blockchain.py

--- Blockchain/blockchain.py
+++ Blockchain/blockchain.py
@@ -2,8 +2,8 @@ from block import Block
 
 class Blockchain:
 
-def _init_ (self):
+    def _init_ (self):
     #we are intializing the blockchain with the genesis Block (first block)
-    self.chain = [self.create_genesis_block()]
-    self.pending_transactions= [] #this is the transactions that need to be mined, starts empty
-    self.difficulty = 4 # this is mining diffculty and this means 4 zeros is required at the end of hash
+        self.chain = [self.create_genesis_block()]
+        self.pending_transactions= [] #this is the transactions that need to be mined, starts empty
+        self.difficulty = 4 # this is mining diffculty and this means 4 zeros is required at the end of hash

2d7af254471b2f6266569fe2052e00043f020265
Knut-commits <26knuto@stu.tes.tp.edu.tw>
2025-03-14 10:27:50 +0800

    finsihed node

--- Blockchain/node.py
+++ Blockchain/node.py
@@ -1,11 +1,13 @@
 #implement a flask based blockchain node
-
+from blockchain import Blockchain
 from flask import Flask, request, jsonify
 
-app = FLask(_name_)
+app = Flask(__name__)
 blockchain = Blockchain()
 
 @app.route('/mine',methods=['GET'])#allows users to access the mine url via a Get request
 def mine():
-    blockchain.mine_pending_transactions("1stMiner")
-    return jsonify({"message"}:)
+    blockchain.mine_pending_transactions("Miner1")
+    return jsonify({"message":"new block mined"})
+if __name__ == '__main__':
+    app.run(debug=True)

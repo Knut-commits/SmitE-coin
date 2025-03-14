6d5b2782cad9e215fbacf647a9112adec90c5e5e
Knut-commits <26knuto@stu.tes.tp.edu.tw>
2025-03-14 11:13:29 +0800

    finished blockchain core

--- Blockchain/block.py
+++ Blockchain/block.py
@@ -3,11 +3,12 @@ import time
 import json #crypto currency libarary.
 #nonce means number only used once and is a cyrpto currency term.
 class Block:
-    def __init__(self,index,previousHash,transactions,timestamp = None,nonce=0):
+    def __init__(self,index,previousHash,transactions,contracts,timestamp = None,nonce=0):
         # Block properties
         self.index = index  # Position of the block in the chain
         self.previousHash = previousHash  # Hash of the previous block
         self.transactions = transactions  # List of transactions in the block
+        self.contracts = contracts #list of contracts in block
         self.timestamp = timestamp or time.time()  # timestamp of when block is created
         self.nonce = nonce  # nonce for Proof of Work
         self.hash = self.calculateHash()  # hash of the block

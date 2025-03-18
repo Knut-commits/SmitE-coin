fa099800f079fe9ad93929487524239de81af757
EdisonHuang810 <edison.huangb2026@stu.tes.tp.edu.tw>
2025-03-18 21:05:47 +0800

    Updated UI 1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// Create a button element
+let button = document.createElement('button');
+
+// Set the button text
+button.textContent = 'Click Me!';
+
+// Add an event listener to the button
+button.addEventListener('click', function() {
+    alert('Button was clicked!');
+});
+let img = Document.createElement('img');
+img.src = 'SmitEcoin.png';
+img.alt = 'Our coin';
+document.body.appendChild(img);

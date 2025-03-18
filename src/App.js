95773db5e18c4b67f57ab1bea90dc04f32b87a56
EdisonHuang810 <edison.huangb2026@stu.tes.tp.edu.tw>
2025-03-18 22:07:51 +0800

    first page of UI done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-// Create a button element
 let button = document.createElement('button');
+button.textContent = 'About us';
+button.id = "OGbutton";
 
-// Set the button text
-button.textContent = 'Click Me!';
-
-// Add an event listener to the button
 button.addEventListener('click', function() {
     alert('Button was clicked!');
 });
-let img = Document.createElement('img');
+
+let button1 = document.createElement('button');
+button1.textContent = "Your Wallet and The exchange";
+button1.id = "button1";
+button1.addEventListener('click', function(){
+    alert('Button 1 was clicked!');
+});
+document.body.appendChild(button1);
+
+document.body.appendChild(button);
+
+let button2 = document.createElement('button');
+button2.textContent = "Your Profile";
+button2.id = "button2";
+button2.addEventListener('click',function(){
+    alert('Button 2 was pressed!');
+})
+document.body.appendChild(button2);
+let img = document.createElement('img');
 img.src = 'SmitEcoin.png';
 img.alt = 'Our coin';
+
 document.body.appendChild(img);
+
+let button3 = document.createElement('button');
+button3.id = "button3";
+button3.textContent = "Log in    Sign Up"
+button3.addEventListener('click', function(){
+    alert('button3 clicked!')
+});
+document.body.appendChild(button3);
+
+let buttonInvest = document.createElement('button');
+buttonInvest.id = "buttonInvest";
+buttonInvest.textContent = "Invest NOW";
+
+buttonInvest.addEventListener('click', function(){
+    alert('Invest NOW opened');
+});
+document.body.appendChild(buttonInvest);
